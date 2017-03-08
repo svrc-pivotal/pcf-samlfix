cee7931bf214878b7724512bf7a42afc0ea0721f
Stuart Charlton <scharlton@pivotal.io>
2017-03-08 14:05:14 -0700

    Edits PCF ERT 1.8 through 1.10 tile metadata to reuse the HAproxy keypair instead of generating a self signed certificate.
    Also sets digital signatures to SHA256.

---
+++ pcf_samlfix.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+display_usage() {
+        echo -e "\nUsage:\npcf_samlfix [path to cf.pivotal file] [destination file]\n"
+        }
+
+# if less than two arguments supplied, display usage
+        if [  $# -le 1 ]
+        then
+                display_usage
+                exit 1
+        fi
+
+
+mkdir tmp
+CF=$1
+DEST=$2
+unzip $CF -d tmp
+sed -i 's/default: SHA1/default: SHA256/' tmp/metadata/cf.yml
+sed -i 's/service_provider_key_credentials\.private_key_pem/\.properties\.networking_point_of_entry\.haproxy\.ssl_rsa_certificate\.private_key_pem/' 
+
+tmp/metadata/cf.yml
+sed -i 's/service_provider_key_credentials\.cert_pem/\.properties\.networking_point_of_entry\.haproxy\.ssl_rsa_certificate\.cert_pem/' tmp/metadata/cf.yml
+pushd tmp
+  zip -r ../$DEST *
+popd
+rm -rf tmp
+exit 0
+
