6e834222d4cf1c56e42d802bab6941ff9a048a37
Stuart Charlton <scharlton@pivotal.io>
2017-03-09 14:23:06 -0700

    Use .bak backup extension

--- pcf_samlfix.sh
+++ pcf_samlfix.sh
@@ -20,7 +20,7 @@ unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
    SED_CMD='sed -i'
 elif [[ "$unamestr" == 'Darwin' ]]; then
-   SED_CMD='sed -i ""'
+   SED_CMD='sed -i ".bak"'
 fi
 $SED_CMD 's/default: SHA1/default: SHA256/' tmp/metadata/cf.yml
 $SED_CMD 's/service_provider_key_credentials\.private_key_pem/\.properties\.networking_point_of_entry\.haproxy\.ssl_rsa_certificate\.private_key_pem/' tmp/metadata/cf.yml
