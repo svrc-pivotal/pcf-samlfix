875873933b839229ec2779bf3e785c53c6454d6b
Stuart Charlton <scharlton@pivotal.io>
2017-03-09 14:06:53 -0700

    Support OS X and Linux

--- pcf_samlfix.sh
+++ pcf_samlfix.sh
@@ -16,10 +16,16 @@ mkdir tmp
 CF=$1
 DEST=$2
 unzip $CF -d tmp
-sed -i 's/default: SHA1/default: SHA256/' tmp/metadata/cf.yml
-sed -i 's/service_provider_key_credentials\.private_key_pem/\.properties\.networking_point_of_entry\.haproxy\.ssl_rsa_certificate\.private_key_pem/' tmp/metadata/cf.yml
-sed -i 's/entityid: "http:/entityid: "https:/' tmp/metadata/cf.yml
-sed -i 's/service_provider_key_credentials\.cert_pem/\.properties\.networking_point_of_entry\.haproxy\.ssl_rsa_certificate\.cert_pem/' tmp/metadata/cf.yml
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   SED_CMD='sed -i'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   SED_CMD='sed -i ""'
+fi
+$SED_CMD 's/default: SHA1/default: SHA256/' tmp/metadata/cf.yml
+$SED_CMD 's/service_provider_key_credentials\.private_key_pem/\.properties\.networking_point_of_entry\.haproxy\.ssl_rsa_certificate\.private_key_pem/' tmp/metadata/cf.yml
+$SED_CMD 's/entityid: "http:/entityid: "https:/' tmp/metadata/cf.yml
+$SED_CMD 's/service_provider_key_credentials\.cert_pem/\.properties\.networking_point_of_entry\.haproxy\.ssl_rsa_certificate\.cert_pem/' tmp/metadata/cf.yml
 pushd tmp
   zip -r ../$DEST *
 popd
